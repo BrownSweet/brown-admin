d2a608130d730ba8ca1560e552d1dd45d3d70e3c
brown_sweet <455764041@qq.com>
2024-11-19 21:20:15 +0800

    新增了测试页面

---
+++ vue3-admin/src/views/test/test.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { getAuths } from "@/router/utils";
+defineOptions({
+  name: "Test"
+});
+</script>
+<template>
+  <div>
+    <h1>测试页面</h1>
+    <p class="mb-2">当前拥有的code列表：{{ getAuths() }}</p>
+    <el-button v-auth="'Test:add'" type="primary">新增</el-button>
+    <el-button v-auth="'Test:delete'" type="warning">删除</el-button>
+  </div>
+</template>
